65708986a9d433ba6ff02dbf519cb382143b3233
shravannalla <nallashravansk@gmail.com>
2026-01-05 20:09:19 +0530

    have common header

--- employee-portal/src/components/pages/EmployeeManagement/index.tsx
+++ employee-portal/src/components/pages/EmployeeManagement/index.tsx
@@ -1,38 +1,16 @@
-import HomePageTemplate from "../../templates/HomePage";
-import Header from "../../organisms/Header";
 import EmployeeManager from "../../organisms/EmployeeManager";
-import { useNavigate } from "react-router-dom";
-import { DASHBOARD, MANAGE_EMPLOYEES } from "../../../utils/constants";
+import { HomePageTemplate } from "../../templates/HomePage";
 
 interface EmployeeManagementProps {
   handleLogout: () => void;
 }
 
 const EmployeeManagement = ({ handleLogout }: EmployeeManagementProps) => {
-  const navigate = useNavigate();
-
-  const handleNavigateToManageScreen = () => {
-    navigate("/employeeManagement");
-  };
-
-  const handleNavigateToDashboard = () => {
-    navigate("/");
-  };
   return (
-    <>
-      <HomePageTemplate
-        content={<EmployeeManager />}
-        header={
-          <Header
-            primaryButtonText={DASHBOARD}
-            secondaryButtonText={MANAGE_EMPLOYEES}
-            handlePrimaryBtnClick={handleNavigateToDashboard}
-            handleSecondaryBtnClick={handleNavigateToManageScreen}
-            handleLogout={handleLogout}
-          />
-        }
-      />
-    </>
+    <HomePageTemplate
+      content={<EmployeeManager />}
+      handleLogout={handleLogout}
+    />
   );
 };
 
--- employee-portal/src/components/templates/HomePage/index.tsx
+++ employee-portal/src/components/templates/HomePage/index.tsx
@@ -1,20 +1,74 @@
+// import { Box, Grid } from "@mui/material";
+// import type { ReactNode } from "react";
+
+// type Props = {
+//   header: ReactNode;
+//   content: ReactNode;
+// };
+
+// export const HomePageTemplate = ({ header, content }: Props) => {
+//   return (
+//     <Grid container direction="column" sx={{ minHeight: "100vh" }} gap={4}>
+//       <Grid item xs={12} height={"10vh"}>
+//         <Box
+//           component="header"
+//           sx={{ borderBottom: "1px solid rgba(0,0,0,0.12)" }}
+//         >
+//           {header}
+//         </Box>
+//       </Grid>
+
+//       <Grid item sx={{
+//           flexGrow: 1,
+//           overflow: 'auto',
+//         }} bgcolor={"#f1f3f0ff"} borderRadius={"8px"} padding={"16px"}>
+//         <Grid container>
+//           <Grid item xs>
+//             {content}
+//           </Grid>
+//         </Grid>
+//       </Grid>
+//     </Grid>
+//   );
+// };
+
+// export default HomePageTemplate;
+
 import { Box, Grid } from "@mui/material";
+import Header from "../../organisms/Header";
+import { DASHBOARD, MANAGE_EMPLOYEES } from "../../../utils/constants";
+import { useNavigate } from "react-router-dom";
 import type { ReactNode } from "react";
 
 type Props = {
-  header: ReactNode;
   content: ReactNode;
+  handleLogout: () => void;
 };
 
-export const HomePageTemplate = ({ header, content }: Props) => {
+export const HomePageTemplate = ({ content, handleLogout }: Props) => {
+  const navigate = useNavigate();
+
+  const handleNavigateToDashboard = () => {
+    navigate("/");
+  };
+
+  const handleNavigateToManageScreen = () => {
+    navigate("/employeeManagement");
+  };
+
   return (
     <Grid container direction="column" sx={{ minHeight: "100vh" }} gap={4}>
       <Grid item xs={12} height={"10vh"}>
         <Box
           component="header"
-          sx={{ borderBottom: "1px solid rgba(0,0,0,0.12)" }}
         >
-          {header}
+          <Header
+            primaryButtonText={DASHBOARD}
+            secondaryButtonText={MANAGE_EMPLOYEES}
+            handlePrimaryBtnClick={handleNavigateToDashboard}
+            handleSecondaryBtnClick={handleNavigateToManageScreen}
+            handleLogout={handleLogout}
+          />
         </Box>
       </Grid>
 
@@ -31,5 +85,3 @@ export const HomePageTemplate = ({ header, content }: Props) => {
     </Grid>
   );
 };
-
-export default HomePageTemplate;
